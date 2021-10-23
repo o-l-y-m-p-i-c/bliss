c56f4f5321e1c6d4bf075bb58346ec269d1c6406
o-l-y-m-p-i-c <73851045+o-l-y-m-p-i-c@users.noreply.github.com>
2021-10-23 21:11:56 +0300

    upload project

---
+++ assets/index.js
@@ -0,0 +1,38 @@
+const headerWrapper = document.querySelector('.header-wrapper');
+const scrollingWatch = {
+    y:''
+}
+window.onload = () => {
+    getScrollY()
+    watchHeader()
+}
+window.addEventListener('scroll', ()=>{
+    getScrollY()
+    watchHeader()
+})
+
+
+function getScrollY(){
+    scrollingWatch.y = document.scrollingElement.scrollTop
+}
+function watchHeader(){
+    if (scrollingWatch.y > 10) {
+        headerWrapper.classList.add('scrolled')
+    }else{
+        headerWrapper.classList.remove('scrolled')
+    }
+}
+
+
+function faqEl(e){
+    // console.log(e.parentElement.children[1].children[0].innerHTML.length);
+    if (e.parentElement.children[1].children[0] && e.parentElement.children[1].children[0].scrollHeight > 0) {
+        let faqElBoll = e.parentElement.classList.toggle("active")
+        if(faqElBoll){
+            e.parentElement.children[1].style.height = `${e.parentElement.children[1].children[0].scrollHeight}px`
+        }else{
+            e.parentElement.children[1].style.height = "0px"
+        }
+    }
+    
+}
